c733d91ea81d23eaf44078947124b0503f421155
Chris <cjmarsh725@gmail.com>
2019-08-24 15:50:12 -0700

    Initial boilerplate added

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">Foo</div>
+    );
+  }
+}
+
+export default App;
